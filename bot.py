880f61f5a7697a1e0569d973030bc4692e16fc1c
unnamedguy <unnamedjguy@icloud.com>
2021-03-29 21:21:49 +0300

    Replace telebot with telethon

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import configparser
 from apitok import API_TOKEN
 from classifier import get_en_news_category
 from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.custom import Button
 from user import User
 
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -28,182 +29,165 @@ client = TelegramClient(username, api_id, api_hash).start()
 
 bot = TelegramClient('CoolStoryBot', api_id, api_hash).start(bot_token=API_TOKEN)
 
-
-@client.on(events.NewMessage())
+@client.on(events.NewMessage)
 async def handler(event):
-    # Respond whenever someone says "Hello" and something else
     await event.reply('aa')
 
+# Keyboard generation
+def create_keyboard():
+    keyboard = [
+        [Button.inline('Sport', 'cb_sport'), Button.inline('World', 'cb_world')],
+        [Button.inline('US', 'cb_us'), Button.inline('Business', 'cb_business')],
+        [Button.inline('Health', 'cb_health'), Button.inline('Entertainment', 'cb_entertainment')],
+        [Button.inline('Science & Tech', 'cb_sci_tech')],
+        [Button.inline('Get news', 'cb_getnews')]
+    ]
+    return keyboard
 
 # Handle '/start' 
 # Welcome message
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, "Hello! I'm CoolstoryBot. What kind of news would you like to receive?", reply_markup=gen_markup())
+@bot.on(events.NewMessage(pattern='/start'))
+async def send_welcome(event):
+    await bot.send_message(event.chat_id, "Hello! I'm CoolstoryBot. What kind of news would you like to receive?", buttons=create_keyboard())
 
-       
-# Handle '/help'
-# Commands list
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.reply_to(message, '\n/start\n/change\n/help') 
-
-
-# Keyboard generation
-def gen_markup():
-    markup = InlineKeyboardMarkup()
-    #markup.row_width = 2
-    markup.row(InlineKeyboardButton('Sport', callback_data='cb_sport'),
-                InlineKeyboardButton('World', callback_data='cb_world'))
-    markup.row(InlineKeyboardButton('US', callback_data='cb_us'),
-                InlineKeyboardButton('Business', callback_data='cb_business'))
-    markup.row(InlineKeyboardButton('Health', callback_data='cb_health'),
-                InlineKeyboardButton('Entertainment', callback_data='cb_entertainment'))
-    markup.add(InlineKeyboardButton('Science & Tech', callback_data='cb_sci_tech'))
-    markup.add(InlineKeyboardButton('Get news', callback_data='cb_getnews'))
-    return markup
-
-
-# Creates keyboard on click
-@bot.message_handler(commands=['change'])
-def start_message2(message):
-    bot.send_message(message.chat.id, 'Categories:', reply_markup=gen_markup())
+@bot.on(events.NewMessage(pattern='/categories'))
+async def send_welcome(event):
+    await bot.send_message(event.chat_id, "Here are the categories:", buttons=create_keyboard())
 
 
 # Handles buttons' clicks
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-    #User initialization
-    new_user = User(call.message.chat.id)
-
-    if call.data == 'cb_sport':
-        markup = call.message.reply_markup
+@bot.on(events.CallbackQuery)
+async def handler(event):
+    # User initialization
+    # new_user = User(event.chat_id)
+    if event.data == b'cb_sport':
+        msg    = await event.get_message()
+        markup = msg.reply_markup
         status = ''
-        if markup.keyboard[0][0].text[-1] == '✅':
-            markup.keyboard[0][0].text = 'Sport'
+        if markup.rows[0].buttons[0].text[-1] == '✅':
+            markup.rows[0].buttons[0].text = 'Sport'
             status = CATEGORY_RM_MSG
-            new_user.selected_categories.remove(call.data[3:])
+            # new_user.selected_categories.remove(event.data[3:])
         else:
-            markup.keyboard[0][0].text = 'Sport ✅'
+            markup.rows[0].buttons[0].text = 'Sport ✅'
             status = CATEGORY_ADD_MSG
-            new_user.selected_categories.append(call.data[3:])
-            
-        bot.edit_message_reply_markup(chat_id=call.message.chat.id, 
-                                        message_id=call.message.id, 
-                                        reply_markup=markup)
-        bot.answer_callback_query(call.id, status)    
-
-    elif call.data == 'cb_world':
-        markup = call.message.reply_markup
-        status = ''
-        if markup.keyboard[0][1].text[-1] == '✅':
-            markup.keyboard[0][1].text = 'World'
-            status = CATEGORY_RM_MSG
-            new_user.selected_categories.remove(call.data[3:])
-        else:
-            markup.keyboard[0][1].text = 'World ✅'
-            status = CATEGORY_ADD_MSG
-            new_user.selected_categories.append(call.data[3:])
-
-        bot.edit_message_reply_markup(chat_id=call.message.chat.id, 
-                                        message_id=call.message.id, 
-                                        reply_markup=markup)
-        bot.answer_callback_query(call.id, status)  
-
-    elif call.data == 'cb_us':
-        markup = call.message.reply_markup
-        status = ''
-        if markup.keyboard[1][0].text[-1] == '✅':
-            markup.keyboard[1][0].text = 'US'
-            status = CATEGORY_RM_MSG
-            new_user.selected_categories.remove(call.data[3:])
-        else:
-            markup.keyboard[1][0].text = 'US ✅'
-            status = CATEGORY_ADD_MSG
-            new_user.selected_categories.append(call.data[3:])
-
-        bot.edit_message_reply_markup(chat_id=call.message.chat.id, 
-                                        message_id=call.message.id, 
-                                        reply_markup=markup)
-        bot.answer_callback_query(call.id, status)
-
-    elif call.data == 'cb_business':
-        markup = call.message.reply_markup
-        status = ''
-        if markup.keyboard[1][1].text[-1] == '✅':
-            markup.keyboard[1][1].text = 'Business'
-            status = CATEGORY_RM_MSG
-            new_user.selected_categories.remove(call.data[3:])
-        else:
-            markup.keyboard[1][1].text = 'Business ✅'
-            status = CATEGORY_ADD_MSG
-            new_user.selected_categories.append(call.data[3:])
-
-        bot.edit_message_reply_markup(chat_id=call.message.chat.id, 
-                                        message_id=call.message.id, 
-                                        reply_markup=markup)
-        bot.answer_callback_query(call.id, status)  
-
-    elif call.data == 'cb_health':
-        markup = call.message.reply_markup
-        status = ''
-        if markup.keyboard[2][0].text[-1] == '✅':
-            markup.keyboard[2][0].text = 'Health'
-            status = CATEGORY_RM_MSG
-            new_user.selected_categories.remove(call.data[3:])
-        else:
-            markup.keyboard[2][0].text = 'Health ✅'
-            status = CATEGORY_ADD_MSG
-            new_user.selected_categories.append(call.data[3:])
-
-        bot.edit_message_reply_markup(chat_id=call.message.chat.id, 
-                                        message_id=call.message.id, 
-                                        reply_markup=markup)
-        bot.answer_callback_query(call.id, status)  
-    
-    elif call.data == 'cb_entertainment':
-        markup = call.message.reply_markup
-        status = ''
-        if markup.keyboard[2][1].text[-1] == '✅':
-            markup.keyboard[2][1].text = 'Entertainment'
-            status = CATEGORY_RM_MSG
-            new_user.selected_categories.remove(call.data[3:])
-        else:
-            markup.keyboard[2][1].text = 'Entertainment ✅'
-            status = CATEGORY_ADD_MSG
-            new_user.selected_categories.append(call.data[3:])
-
-        bot.edit_message_reply_markup(chat_id=call.message.chat.id, 
-                                        message_id=call.message.id, 
-                                        reply_markup=markup)
-        bot.answer_callback_query(call.id, status)  
+            # new_user.selected_categories.append(event.data[3:])
         
-    elif call.data == 'cb_sci_tech':
-        markup = call.message.reply_markup
-        status = ''
-        if markup.keyboard[3][0].text[-1] == '✅':
-            markup.keyboard[3][0].text = 'Science & Tech'
-            status = CATEGORY_RM_MSG
-            new_user.selected_categories.remove(call.data[3:])
-        else:
-            markup.keyboard[3][0].text = 'Science & Tech ✅'
-            status = CATEGORY_ADD_MSG
-            new_user.selected_categories.append(call.data[3:])
+        await msg.edit(buttons=markup)
 
-        bot.edit_message_reply_markup(chat_id=call.message.chat.id, 
-                                        message_id=call.message.id, 
-                                        reply_markup=markup)
-        bot.answer_callback_query(call.id, status)  
+        await event.answer(status)    
+
+    elif event.data == b'cb_world':
+        msg    = await event.get_message()
+        markup = msg.reply_markup
+        status = ''
+        if markup.rows[0].buttons[1].text[-1] == '✅':
+            markup.rows[0].buttons[1].text = 'World'
+            status = CATEGORY_RM_MSG
+            # new_user.selected_categories.remove(event.data[3:])
+        else:
+            markup.rows[0].buttons[1].text = 'World ✅'
+            status = CATEGORY_ADD_MSG
+            # new_user.selected_categories.append(event.data[3:])
+
+        await msg.edit(buttons=markup) 
+                                                                                
+        await event.answer(status)  
+
+    elif event.data == b'cb_us':
+        msg    = await event.get_message()
+        markup = msg.reply_markup
+        status = ''
+        if markup.rows[1].buttons[0].text[-1] == '✅':
+            markup.rows[1].buttons[0].text = 'US'
+            status = CATEGORY_RM_MSG
+            # new_user.selected_categories.remove(event.data[3:])
+        else:
+            markup.rows[1].buttons[0].text = 'US ✅'
+            status = CATEGORY_ADD_MSG
+            # new_user.selected_categories.append(event.data[3:])
+
+        await msg.edit(buttons=markup) 
+                                                                                
+        await event.answer(status)
+
+    elif event.data == b'cb_business':
+        msg    = await event.get_message()
+        markup = msg.reply_markup
+        status = ''
+        if markup.rows[1].buttons[1].text[-1] == '✅':
+            markup.rows[1].buttons[1].text = 'Business'
+            status = CATEGORY_RM_MSG
+            # new_user.selected_categories.remove(event.data[3:])
+        else:
+            markup.rows[1].buttons[1].text = 'Business ✅'
+            status = CATEGORY_ADD_MSG
+            # new_user.selected_categories.append(event.data[3:])
+
+        await msg.edit(buttons=markup) 
+                                                                                
+        await event.answer(status)  
+
+    elif event.data == b'cb_health':
+        msg    = await event.get_message()
+        markup = msg.reply_markup
+        status = ''
+        if markup.rows[2].buttons[0].text[-1] == '✅':
+            markup.rows[2].buttons[0].text = 'Health'
+            status = CATEGORY_RM_MSG
+            # new_user.selected_categories.remove(event.data[3:])
+        else:
+            markup.rows[2].buttons[0].text = 'Health ✅'
+            status = CATEGORY_ADD_MSG
+            # new_user.selected_categories.append(event.data[3:])
+
+        await msg.edit(buttons=markup) 
+                                                                              
+        await event.answer(status)  
+    
+    elif event.data == b'cb_entertainment':
+        msg    = await event.get_message()
+        markup = msg.reply_markup
+        status = ''
+        if markup.rows[2].buttons[1].text[-1] == '✅':
+            markup.rows[2].buttons[1].text = 'Entertainment'
+            status = CATEGORY_RM_MSG
+            # new_user.selected_categories.remove(event.data[3:])
+        else:
+            markup.rows[2].buttons[1].text = 'Entertainment ✅'
+            status = CATEGORY_ADD_MSG
+            # new_user.selected_categories.append(event.data[3:])
+
+        await msg.edit(buttons=markup) 
+                                                                                
+        await event.answer(status)  
+        
+    elif event.data == b'cb_sci_tech':
+        msg    = await event.get_message()
+        markup = msg.reply_markup
+        status = ''
+        if markup.rows[3].buttons[0].text[-1] == '✅':
+            markup.rows[3].buttons[0].text = 'Science & Tech'
+            status = CATEGORY_RM_MSG
+            # new_user.selected_categories.remove(event.data[3:])
+        else:
+            markup.rows[3].buttons[0].text = 'Science & Tech ✅'
+            status = CATEGORY_ADD_MSG
+            # new_user.selected_categories.append(event.data[3:])
+
+        await msg.edit(buttons=markup) 
+                                                                                
+        await event.answer(status)  
 
     else:
-        bot.answer_callback_query(call.id, 'Got the news!')
-        
-        #Add new user to the list
-        if call.message.chat.id not in id_list:
-            new_user.subscribed = True #start sending news
-            id_list.append(new_user.chat_id)
-            user_list.append(new_user)
+        await event.answer('Got the news!')
 
-bot.polling(none_stop=True)
+    # #Add new user to the list
+    # if call.message.chat.id not in id_list:
+    #     new_user.subscribed = True #start sending news
+    #     id_list.append(new_user.chat_id)
+    #     user_list.append(new_user)
+
+
 client.run_until_disconnected()
 bot.run_until_disconnected()        
