b0b28d1bcc604737c6efe2d1d0cc1cec3372b5ce
unnamedguy <unnamedjguy@icloud.com>
2021-03-30 16:42:45 +0300

    Replace double quotes with single quotes in classifier.py

--- classifier.py
+++ classifier.py
@@ -3,14 +3,14 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
 def get_en_news_category(headline):
-    category_list = ["sport", "world", "us", "business", "health", "entertainment", "sci_tech"]
+    category_list = ['sport', 'world', 'us', 'business', 'health', 'entertainment', 'sci_tech']
 
     docs_new = [headline]
 
     #LOAD MODEL
-    loaded_vec = CountVectorizer(vocabulary=pickle.load(open("count_vector.pkl", "rb")))
-    loaded_tfidf = pickle.load(open("tfidf.pkl","rb"))
-    loaded_model = pickle.load(open("softmax.pkl","rb"))
+    loaded_vec = CountVectorizer(vocabulary=pickle.load(open('count_vector.pkl', 'rb')))
+    loaded_tfidf = pickle.load(open('tfidf.pkl','rb'))
+    loaded_model = pickle.load(open('softmax.pkl','rb'))
 
     X_new_counts = loaded_vec.transform(docs_new)
     X_new_tfidf = loaded_tfidf.transform(X_new_counts)
