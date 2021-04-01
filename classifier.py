fad78aee7377214d0f38042f9cee3c59e1f02a0c
Andreenko Aleksandr <andreenko55@gmail.com>
2021-04-02 00:30:01 +0300

    Add working ru classifier

--- classifier.py
+++ classifier.py
@@ -8,12 +8,26 @@ def get_en_news_category(headline):
     docs_new = [headline]
 
     #LOAD MODEL
-    loaded_vec = CountVectorizer(vocabulary=pickle.load(open('count_vector.pkl', 'rb')))
-    loaded_tfidf = pickle.load(open('tfidf.pkl','rb'))
-    loaded_model = pickle.load(open('softmax.pkl','rb'))
+    loaded_vec = CountVectorizer(vocabulary=pickle.load(open('en_classifier/count_vector.pkl', 'rb')))
+    loaded_tfidf = pickle.load(open('en_classifier/tfidf.pkl','rb'))
+    loaded_model = pickle.load(open('en_classifier/softmax.pkl','rb'))
 
     X_new_counts = loaded_vec.transform(docs_new)
     X_new_tfidf = loaded_tfidf.transform(X_new_counts)
     predicted = loaded_model.predict(X_new_tfidf)
+    return category_list[predicted[0]]
 
+def get_ru_news_category(headline):
+    category_list = ['Бизнес', 'Интернет и СМИ', 'Культура', 'Мир', 'Наука и техника', 'Россия', 'Спорт']
+
+    docs_new = [headline]
+
+    #LOAD MODEL
+    loaded_vec = CountVectorizer(vocabulary=pickle.load(open('ru_classifier/count_vector_title.pkl', 'rb')))
+    loaded_tfidf = pickle.load(open('ru_classifier/tfidf_title.pkl','rb'))
+    loaded_model = pickle.load(open('ru_classifier/softmax_title.pkl','rb'))
+
+    X_new_counts = loaded_vec.transform(docs_new)
+    X_new_tfidf = loaded_tfidf.transform(X_new_counts)
+    predicted = loaded_model.predict(X_new_tfidf)
     return category_list[predicted[0]]
