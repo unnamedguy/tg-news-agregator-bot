a0f441a78d2fc5ed4e32a0460aa82b3e7c46ea43
Andreenko Aleksandr <andreenko55@gmail.com>
2021-04-03 18:59:32 +0300

    Add language selection to keyboard

--- bot.py
+++ bot.py
@@ -24,11 +24,25 @@ username = config['Telegram']['username']
 client = TelegramClient(username, api_id, api_hash).start()
 
 bot = TelegramClient('CoolStoryBot', api_id, api_hash).start(bot_token=API_TOKEN)
+
 @client.on(events.NewMessage(chats=[-1001317428262]))
-async def handler(event):
+async def news_handler(event):
     await client.forward_messages(1436454776, event.message)
 
-# Keyboard generation
+# Language keyboard generation
+def create_lang_keyboard(chat_id):
+    user_entry=[]
+    with closing(psycopg2.connect(dbname=DB_NAME, user=USER, password=PASS, host=HOST)) as conn:
+        with conn.cursor() as cursor:
+            conn.autocommit = True
+            cursor.execute(f'SELECT * FROM bot_user WHERE chat_id = {chat_id}')
+            user_entry = cursor.fetchall()
+    en = ' ✅' if user_entry[0][-1]==True else ''
+    ru = ' ✅' if user_entry[0][-1]==False else ''
+    keyboard = [Button.inline(f'Русский{ru}', 'cb_ru'), Button.inline(f'English{en}', 'cb_en')]
+    return keyboard
+
+# Categories keyboard generation
 def create_keyboard(chat_id):
     user_entry = []
     with closing(psycopg2.connect(dbname=DB_NAME, user=USER, password=PASS, host=HOST)) as conn:
@@ -36,13 +50,15 @@ def create_keyboard(chat_id):
             conn.autocommit = True
             cursor.execute(f'SELECT * FROM bot_user WHERE chat_id = {chat_id}')
             user_entry = cursor.fetchall()
-    ctgrs = [' ✅' if i==True else '' for i in user_entry[0]]
+    ctgrs = [' ✅' if user_entry[0][i]==True else '' for i in range(0, 7)]
+    en = ' ✅' if user_entry[0][-1]==True else ''
+    ru = ' ✅' if user_entry[0][-1]==False else ''
     keyboard = [
         [Button.inline(f'Sport{ctgrs[0]}', 'cb_sport'), Button.inline(f'World{ctgrs[1]}', 'cb_world')],
         [Button.inline(f'US{ctgrs[2]}', 'cb_us'), Button.inline(f'Business{ctgrs[3]}', 'cb_business')],
         [Button.inline(f'Health{ctgrs[4]}', 'cb_health'), Button.inline(f'Entertainment{ctgrs[5]}', 'cb_entertainment')],
         [Button.inline(f'Science & Tech{ctgrs[6]}', 'cb_sci_tech')],
-        [Button.inline('Get news', 'cb_getnews')]
+        [Button.inline(f'Русский{ru}', 'cb_ru'), Button.inline(f'English{en}', 'cb_en')]
     ]
     return keyboard
 
@@ -78,11 +94,9 @@ async def client_handler(event):
             for user in sub_users:
                 await bot.forward_messages(user[0], event.message)
 
-
-
 # Handles buttons' clicks
 @bot.on(events.CallbackQuery)
-async def handler(event):
+async def keyboard_handler(event):
     with closing(psycopg2.connect(dbname=DB_NAME, user=USER, password=PASS, host=HOST)) as conn:
         with conn.cursor() as cursor:
             conn.autocommit = True
@@ -93,7 +107,9 @@ async def handler(event):
                     b'cb_business'     : 'Business',
                     b'cb_health'       : 'Health',
                     b'cb_entertainment': 'Entertainment',
-                    b'cb_sci_tech'     : 'Science & Tech'}
+                    b'cb_sci_tech'     : 'Science & Tech',
+                    b'cb_ru'           : 'Русский',
+                    b'cb_eb'           : 'English'}
             
             pos = 0
             for key, name in btns.items():
@@ -116,9 +132,6 @@ async def handler(event):
                     await event.answer(status)
                     break
                 pos += 1
-            
-            if pos >= 7:
-                await event.respond("Done! I'll keep you up to date!")
 
 client.run_until_disconnected()
 bot.run_until_disconnected()        
